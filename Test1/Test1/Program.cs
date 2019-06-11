0014c3cb3f8d57525b56995086d4f9b282a785b4
zanebridle18@gmail.com <zanebridle18@gmail.com>
2019-06-11 14:55:00 +0200

    Initial commit

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Test1
+{
+    class Program
+    {
+        public static int Sum(int a,int b)
+        {
+            int total;
+            total = a + b;
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter a number: ");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            
+            int a = new int();
+
+            
+            Console.Write("Enter another number: ");
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int b = new int();
+
+            Console.WriteLine("total: " + (a1 + b1));
+          
+            
+        }
+    }
+}
